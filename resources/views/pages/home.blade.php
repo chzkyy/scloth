86cb2e99b745fbb59cb34e3f018372ce45b696c1
chzkyy <ftrizkyy@gmail.com>
2022-01-07 20:57:51 +0700

    be homepage

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,184 @@
+@extends('layouts.app')
+
+@section('title')
+    Home
+@endsection
+
+@section('content')
+    {{--  header  --}}
+    <header class="text-center">
+        <h1>
+           The last Collection of the year
+        </h1>
+        <p class="mt-3">
+            Here you can find the last collection of the year
+        </p>
+        <a href="#" class="btn btn-get-started px-4 mt-4">
+            Shop Now
+        </a>
+    </header>
+    {{--  akhir header  --}}
+    
+    <main>
+
+        {{--  Stats section  --}}
+        <div class="container">
+            <section class="section-stats row justify-content-center" id="stats">
+                <div class="col-4 col-md-2 stats-detail text-center">
+                    <i class="fas fa-truck"></i>
+                    <p>Free Shipping</p>
+                </div>
+                <div class="col-4 col-md-2 stats-detail text-center">
+                    <i class="fas fa-headset"></i>
+                    <p>Support 24/7</p>
+                </div>
+                <div class="col-4 col-md-2 stats-detail text-center">
+                    <i class="fas fa-sync-alt"></i>
+                    <p>30Days return</p>
+                </div>
+            </section>
+        </div>
+        {{--  akhir stats  --}}
+
+        {{--  category section  --}}
+        <section class="section-popular" id="popular">
+            <div class="container">
+                <div class="row">
+                    <div class="col text-center section-popular-heading">
+                        <h2>New Apparel</h2>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        {{--  isi category  --}}
+        <section class="section-popular-content" id="popularContent">
+            <div class="container">
+                <div class="section-popular-shop row justify-content-center">
+
+                    <div class="col-sm-6 col-md-4 col-lg-3">
+                        <div class="card-shop text-center d-flex flex-column"
+                            style="background-image: url('frontend/images/pi1.jpg');">
+                            <div class="shop-button mt-auto">
+                                <div class="title-product bg-light">Category 1</div>
+                                <a href="#" class="btn btn-shop-details mt-4 px-4">
+                                    View Details
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-sm-6 col-md-4 col-lg-3">
+                        <div class="card-shop text-center d-flex flex-column"
+                            style="background-image: url('frontend/images/pi2.jpg');">
+                            <div class="shop-button mt-auto">
+                                <div class="title-product bg-light">Category 2</div>
+                                <a href="#" class="btn btn-shop-details mt-4 px-4">
+                                    View Details
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-sm-6 col-md-4 col-lg-3">
+                        <div class="card-shop text-center d-flex flex-column"
+                            style="background-image: url('frontend/images/pi3.jpg');">
+                            <div class="shop-button mt-auto">
+                                <div class="title-product bg-light">Category 3</div>
+                                <a href="#" class="btn btn-shop-details mt-4 px-4">
+                                    View Details
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-sm-6 col-md-4 col-lg-3">
+                        <div class="card-shop text-center d-flex flex-column"
+                            style="background-image: url('frontend/images/pi4.jpg');">
+                            <div class="shop-button mt-auto">
+                                <div class="title-product bg-light">Category 4</div>
+                                <a href="#" class="btn btn-shop-details mt-4 px-4">
+                                    View Details
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </section>
+        {{--  akhir isi category  --}}
+
+        {{--  testimonial  --}}
+        <section class="section-testimonials-heading" id="testimonialsHeading">
+            <div class="container">
+                <div class="row">
+                    <div class="col text-center">
+                        <h2>Testimonial</h2>
+                        <p>
+                            Moments were giving them
+                            <br />
+                            the best experience
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        {{--  isi testimonial  --}}
+        <section class="section-testimonials-content" id="testimonialsContent">
+            <div class="container">
+                <div class="section-popular-shop row justify-content-center match-height">
+                    
+                    <div class="col-sm-6 col-md-6 col-lg-4">
+                        <div class="card card-testimonial text-center">
+                            <div class="testimonial-content">
+                                <h3 class="mb-4">Lorem ipsum</h3>
+                                <p class="testimonials">
+                                    “ Lorem ipsum dolor sit amet consectetur adipisicing elit. “
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-sm-6 col-md-6 col-lg-4">
+                        <div class="card card-testimonial text-center">
+                            <div class="testimonial-content">
+                                <h3 class="mb-4">Lorem ipsum</h3>
+                                <p class="testimonials">
+                                    “Lorem ipsum dolor sit amet consectetur adipisicing elit.“
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-sm-6 col-md-6 col-lg-4">
+                        <div class="card card-testimonial text-center">
+                            <div class="testimonial-content mb-auto">
+                                <h3 class="mb-4">Lorem ipsum</h3>
+                                <p class="testimonials">
+                                    “Lorem ipsum dolor sit amet consectetur adipisicing elit.“
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                {{--  akhir isi testimonial  --}}
+                <div class="row">
+                    <div class="col-12 text-center">
+                        <a href="#" class="btn btn-need-help px-4 mt-5 mx-1">
+                            I Need Help
+                        </a>
+                        <a href="#" class="btn btn-get-started px-4 mt-5 mx-1">
+                            Get Started
+                        </a>
+                    </div>
+                </div>
+
+            </div>
+        </section>
+
+        {{--  akhir testimonial  --}}
+    </main>
+@endsection
